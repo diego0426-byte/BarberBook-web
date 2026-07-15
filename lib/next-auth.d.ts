595b02ec30b0a52fe95f46c7ca1bdbe01163a5ba
Luis Diego Ramirez <diego-04-26@gmail.com>
2026-07-15 17:38:44 -0600

    Fix NextAuth types

---
+++ lib/next-auth.d.ts
@@ -0,0 +1,19 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    user: DefaultSession["user"] & {
+      id?: string;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken?: string;
+    refreshToken?: string;
+    expiresAt?: number;
+  }
+}
